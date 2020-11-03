48be646ec623a493691fda1a299bb7fc10b6a408
penwith <codeglide@gmail.com>
2020-11-03 19:51:17 +0000

    Vector

--- code/game.js
+++ code/game.js
@@ -45,8 +45,17 @@ function Level(plan) {
 
     this.status = this.finishDelay = null;
 }
-
 Level.prototype.isFinished = function() {
     return this.status != null && this.finishDelay < 0;
+}
+
+function Vector(x, y) {
+    this.x = x; this.y = y;
+}
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+}
+Vector.prototype.times = function(factor) {
+    return new Vector(this.x * factor, this.y * factor);
 }
 
