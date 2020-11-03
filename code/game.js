94b1a4f9af65d7c489e5d486ce9516b66dfa2b6b
penwith <codeglide@gmail.com>
2020-11-03 19:02:48 +0000

    Level function

--- code/game.js
+++ code/game.js
@@ -9,3 +9,39 @@ var simpleLevelPlan = [
     "      xxxxxxxxxxxxxx  ",
     "                      "
 ];
+
+function Level(plan) {
+
+    this.width = plan[0].length;
+    this.height = plan.length;
+    this.grid = [];
+    this.actors = [];
+    
+    for (var y = 0; y < this.height; y++) {
+
+        var line = plan[y], gridline = [];
+        
+        for (var x = 0; x < this.width; x++) {
+
+            var ch = line[x], fieldType = null;
+            var Actor = actorChars[ch];
+
+            if (Actor)
+                this.actors.push(new Actor(new Vector(x, y), ch));
+            else if (ch == "x")
+                fieldType = "wall";
+            else if (ch == "!")
+                fieldType = "lava";
+
+            gridline.push(fieldType);
+        }
+
+        this.grid.push(gridline);
+    }
+
+    this.player = this.actors.filter(function(actor) {
+        return actor.type == "player";
+    })[0];
+
+    this.status = this.finishDelay = null;
+}
