ff1301dd6fe5cc6bacab50259fc1f94284b0233e
penwith <codeglide@gmail.com>
2020-11-03 20:02:12 +0000

    Lava

--- code/game.js
+++ code/game.js
@@ -74,3 +74,20 @@ function Player(pos, ch) {
 }
 Player.prototype.type = "player";
 
+function Lava(pos, ch) {
+
+    this.pos = pos;
+    this.size = new Vector(1, 1);
+
+    if (ch == "=") {
+        this.speed = new Vector(2, 0);
+    }
+    else if (ch == "|") {
+        this.speed = new Vector(0, 2);
+    }
+    else if (ch == "v") {
+        this.speed = new Vector(0, 3);
+        this.repeatpos = pos;
+    }
+}
+Player.prototype.type = "lava";
