6123e1c993b90c6c5b98b8eca169c2aeb2e8c5f7
penwith <codeglide@gmail.com>
2020-11-04 21:13:23 +0000

    implement move left and right for player

--- code/game.js
+++ code/game.js
@@ -265,3 +265,22 @@ Coin.prototype.act = function(step) {
     this.pos = this.basePos.plus(new Vector(0, wobblePos));
 };
 
+var playerXSpeed = 7;
+
+Player.prototype.moveX = function(step, level, keys) {
+
+    this.speed.x = 0;
+
+    if (keys.left) this.speed.x -= playerXSpeed;
+    if (keys.right) this.speed.x += playerXSpeed;
+
+    var motion = new Vector(this.speed.x * step, 0);
+    var newPos = this.pos.plus(motion);
+    var obstacle = level.obstacleAt(newPos, this.size);
+
+    if (obstacle)
+        level.playerTouched(obstacle);
+    else
+        this.pos = newPos;
+};
+
