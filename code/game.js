4bf27788da7e46dad7981c6638dbeb02520830ea
penwith <codeglide@gmail.com>
2020-11-04 21:21:21 +0000

    implement move up and down for player

--- code/game.js
+++ code/game.js
@@ -284,3 +284,27 @@ Player.prototype.moveX = function(step, level, keys) {
         this.pos = newPos;
 };
 
+var gravity = 30;
+var jumpSpeed = 17;
+
+Player.prototype.MoveY = function(step, level, keys) {
+
+    this.speed.y += step * gravity;
+
+    var motion = new Vector(0, this.speed.y * step);
+    var newPos = this.pos.plus(motion);
+    var obstacle = level.obstacleAt(newPos, this.size);
+
+    if (obstacle) {
+
+        level.playerTouched(obstacle);
+
+        if (keys.up && this.speed.y > 0)
+            this.speed.y = -jumpSpeed;
+        else
+            this.speed.y = 0;
+    } else {
+        this.pos = newPos;
+    }
+};
+
