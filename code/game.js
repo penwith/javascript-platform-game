049afc8dd3680ee15770fc1933c65804eef2f8ea
penwith <codeglide@gmail.com>
2020-11-04 19:48:44 +0000

    implement act for lava

--- code/game.js
+++ code/game.js
@@ -87,7 +87,7 @@ function Lava(pos, ch) {
     }
     else if (ch == "v") {
         this.speed = new Vector(0, 3);
-        this.repeatpos = pos;
+        this.repeatPos = pos;
     }
 }
 Player.prototype.type = "lava";
@@ -243,5 +243,17 @@ Level.prototype.animate = function(step, keys) {
 
         step -= thisStep;
     }
+};
+
+Lava.prototype.act = function(step, level) {
+
+    var newPos = this.pos.plus(this.speed.times(step));
+    
+    if (!level.obstacleAt(newPos, this.size))
+        this.pos = newPos;
+    else if (this.repeatPos)
+        this.pos = this.repeatPos;
+    else
+        this.speed = this.speed.times(-1);
 };
 
